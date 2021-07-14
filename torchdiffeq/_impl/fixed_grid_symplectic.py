8995861319c08447da5644fc0a4c7a402cf6deba
Kiran Bacsa <kbacsa-ethz>
2021-07-14 10:02:21 +0800

    replace matmul with batchwise matmul

--- torchdiffeq/_impl/fixed_grid_symplectic.py
+++ torchdiffeq/_impl/fixed_grid_symplectic.py
@@ -84,10 +84,10 @@ class VelocityVerlet(SymplecticSolver):
         m_1 = torch.eye(n)
 
         k_ = func(t + self.eps, y[..., :n])
-        dy[..., :n] = h * (y[..., n:] - 0.5 * h * torch.matmul(m_1, k_))
+        dy[..., :n] = h * (y[..., n:] - 0.5 * h * torch.nn.functional.linear(k_, m_1))
 
         k_ += func(t + self.eps, y[..., :n] + dy[..., :n])
-        dy[..., n:] = - 0.5 * h * torch.matmul(m_1, k_)
+        dy[..., n:] = - 0.5 * h * torch.nn.functional.linear(k_, m_1)
         return dy
 
 
